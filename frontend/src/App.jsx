3649bbe19065371636672f6e4d3c860a4ede94dd
volequyen <your-email@example.com>
2025-05-16 21:04:09 +0700

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { publicRoutes } from './routers'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <div className='App'>
+         <Routes>
+              {publicRoutes.map((route, index)=> {
+                  const Page = route.componet;
+                  return <Route key={index} path={route.path} element={<Page/>}/>;
+              })}
+         </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/routers/index.js
@@ -0,0 +1,8 @@
+
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home"
+
+const privateRoutes = []
+const publicRoutes = []
+
+export {publicRoutes, privateRoutes}
